7f4feced15915a86a46d8bd4b44012575583c2e3
CaptainMemes <ALEJANDRODAMAS@live.seminolestate.edu>
2018-01-20 23:17:31 -0600

    Filter screen

--- navigation/RootNavigation.js
+++ navigation/RootNavigation.js
@@ -4,6 +4,7 @@ import { StackNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
 import HomeScreen from '../screens/HomeScreen';
+import FilterScreen from '../screens/FilterScreen';
 import registerForPushNotificationsAsync from '../api/registerForPushNotificationsAsync';
 
 const RootStackNavigator = StackNavigator(
