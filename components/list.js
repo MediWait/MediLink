1c23496d7a851a69315a1e854bcac0b73b772b2a
René Léveillé <game2world@yahoo.ca>
2018-01-21 10:35:00 -0500

    added to list

--- components/list.js
+++ components/list.js
@@ -32,5 +32,37 @@ export const hospitals = [
 		time: '2h37',
 		crowd: '25',
 		color: Colors.green
-	}
+	},
+	{
+		id: 5,
+		name: 'Institut de Readaptation Lindsay-gingras',
+		distance: '7.5 km',
+		time: '1h00',
+		crowd: '5',
+		color: Colors.green
+	},
+	{
+		id: 6,
+		name: "St. Mary's Hospital",
+		distance: '5.5 km',
+		time: '4h00',
+		crowd: '56',
+		color: Colors.yellow
+	},
+	{
+		id: 7,
+		name: 'Montreal General Hospital',
+		distance: '10.1 km',
+		time: '7h35',
+		crowd: '30',
+		color: Colors.red
+	},
+	{
+		id: 8,
+		name: "Montreal Children's Hospital",
+		distance: '11.4 km',
+		time: '0h55',
+		crowd: '5',
+		color: Colors.green
+	},
 ];
